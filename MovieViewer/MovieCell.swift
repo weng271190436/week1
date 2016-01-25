b0363ae3c1c03f6b1f968fc853c36870a846ff4a
Wei Weng <wengwei01@gmail.com>
2016-01-25 17:15:47 -0600

    second commit

---
+++ MovieViewer/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by WENG on 16/1/25.
+//  Copyright © 2016年 Wei Weng. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
